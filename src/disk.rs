de941491bf63317a6c912834491aafdd8c9919d4
p1ned0g <ken898425ken@gmail.com>
2026-09-17 23:18:28 +0900

    fix some methods and tests about diskMng

--- src/disk.rs
+++ src/disk.rs
@@ -2,6 +2,15 @@ use std::{fs::{File, OpenOptions}, io::{self, Read, Seek, Write}, path::Path};
 
 const PAGE_SIZE: u64 = 4096;
 
+#[derive(Clone, Copy)]
+pub struct PageId(u64);
+
+impl PageId{
+    pub fn to_u64(&self) -> u64 {
+        self.0
+    }
+}
+
 pub struct DiskManager {
     heap_file: File,
     next_page_id: u64,
@@ -14,20 +23,26 @@ impl DiskManager {
         Ok(Self { heap_file, next_page_id })
     }
 
-    pub fn open(path: impl AsRef<Path>) -> io::Result<(Self)> {
+    pub fn open(path: impl AsRef<Path>) -> io::Result<Self> {
         let heap_file = OpenOptions::new().read(true).write(true).create(true).open(path)?;
         Self::new(heap_file)
     }
 
-    pub fn read_page_data(&mut self, page_id: u64, buf: &mut [u8]) -> io::Result<()> {
-        let pos = page_id * PAGE_SIZE;
+    pub fn allocate_page(&mut self) -> PageId {
+        let page_id = PageId(self.next_page_id);
+        self.next_page_id += 1;
+        page_id
+    }
+
+    pub fn read_page_data(&mut self, page_id: PageId, buf: &mut [u8]) -> io::Result<()> {
+        let pos = page_id.to_u64() * PAGE_SIZE;
         self.heap_file.seek(io::SeekFrom::Start(pos))?;
         self.heap_file.read_exact(buf)?;
         Ok(())
     }
 
-    pub fn write_page_data(&mut self, page_id: u64, buf: &[u8]) -> io::Result<()>{
-        let pos = page_id * PAGE_SIZE;
+    pub fn write_page_data(&mut self, page_id: PageId, buf: &[u8]) -> io::Result<()>{
+        let pos = page_id.to_u64() * PAGE_SIZE;
         self.heap_file.seek(io::SeekFrom::Start(pos))?;
         self.heap_file.write_all(buf)?;
         Ok(())
@@ -37,37 +52,56 @@ impl DiskManager {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::fs::OpenOptions;
-
-    const FILE_NAME :&str = "mydb.db";
 
     #[test]
     fn test_disk_manager() {
-        let _file = File::create(FILE_NAME).unwrap();
+        let path = "test.db";
+        let _ = std::fs::remove_file(path);
 
-        let _ = std::fs::remove_file(FILE_NAME);
-
-        // 新しく作成
-        let file = OpenOptions::new()
-            .read(true)
-            .write(true)
-            .create(true)
-            .open(FILE_NAME).unwrap();
-
-        let mut disk = DiskManager::new(file).unwrap();
+        let mut dskmng = DiskManager::open(path).unwrap();
 
         // pattern 1
-        let data = b"Hello, world!";
-        disk.write_page_data(0, data).unwrap();
-        let mut buf = vec![0u8; data.len()];
-        disk.read_page_data(0, &mut buf).unwrap();
+        let page0 = dskmng.allocate_page();
+        let data1 = b"Hello, world!";
+        dskmng.write_page_data(page0,data1).unwrap();
+        let mut buf = vec![0u8; data1.len()];
+        dskmng.read_page_data(page0, &mut buf).unwrap();
         assert_eq!(String::from_utf8_lossy(&buf), "Hello, world!");
-
+        
+        
         // pattern 2
-        let data = b"I'm, P1ned0g!!";
-        disk.write_page_data(1, data).unwrap();
-        let mut buf = vec![0u8; data.len()];
-        disk.read_page_data(1, &mut buf).unwrap();
+        let page1 = dskmng.allocate_page();
+        let data2 = b"I'm, P1ned0g!!";
+        dskmng.write_page_data(page1,data2).unwrap();
+        let mut buf = vec![0u8; data2.len()];
+        dskmng.read_page_data(page1, &mut buf).unwrap();
         assert_eq!(String::from_utf8_lossy(&buf), "I'm, P1ned0g!!");
+
+
+        // let _file = File::create(FILE_NAME).unwrap();
+
+        // let _ = std::fs::remove_file(FILE_NAME);
+
+        // let file = OpenOptions::new()
+        //     .read(true)
+        //     .write(true)
+        //     .create(true)
+        //     .open(FILE_NAME).unwrap();
+
+        // let mut disk = DiskManager::new(file).unwrap();
+
+        // // pattern 1
+        // let data = b"Hello, world!";
+        // disk.write_page_data(0, data).unwrap();
+        // let mut buf = vec![0u8; data.len()];
+        // disk.read_page_data(0, &mut buf).unwrap();
+        // assert_eq!(String::from_utf8_lossy(&buf), "Hello, world!");
+
+        // // pattern 2
+        // let data = b"I'm, P1ned0g!!";
+        // disk.write_page_data(1, data).unwrap();
+        // let mut buf = vec![0u8; data.len()];
+        // disk.read_page_data(1, &mut buf).unwrap();
+        // assert_eq!(String::from_utf8_lossy(&buf), "I'm, P1ned0g!!");
     }
 }
